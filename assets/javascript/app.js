65508f396fd33ef9eaf98f92f504d0f9605c3bc7
Perri Specter <plspecter@gmail.com>
2018-09-19 11:10:53 -0400

    Crystal-Collector

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,12 +35,37 @@ $(document).ready(function () {
 
     //set variables for wins and losses 
     var wins = 0;
-    var losses =0;
+    var losses = 0;
 
     //Functions
     //=====================
 
-    
+    //Set the computer to calculate a random number between..
+    //1 - 12 to use as the crystal's values
+
+    var getRandom = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    //Now the getRandom is a number random generator
+
+    //Start the game function
+
+    var startGame = function () {
+
+    //current score starts at 0
+    currentScore = 0;
+
+    //set a new target score - the get random generator is called to insert 19, 120 for min and max
+    targetScore = getRandom (19, 120);
+
+    //go into the var crystals and target their values to change
+    crystal.green.value = getRandom (1, 12);
+
+
+    }
+
+
 
 
 });
