a03c32aafaf99299ca977c3dbfbe2539f79a0ced
Perri Specter <plspecter@gmail.com>
2018-09-19 10:52:42 -0400

    Crystal-Collector

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+console.log("connected");
+
+$(document).ready(function () {
+
+    //target each gem as a random value
+
+    //crystal variables
+    var crystals = {
+
+        green: {
+            name: "green",
+            value = 0
+        },
+
+        blue: {
+            name: "blue",
+            value = 0
+        },
+
+        pink: {
+            name: "pink",
+            value = 0
+        },
+
+        orange: {
+            name: "orange",
+            value = 0
+        }
+
+    }
+
+    //set variables for current and target scores
+    var currentScore = 0;
+    var targetScore = 0;
+
+    //set variables for wins and losses 
+    var wins = 0;
+    var losses =0;
+
+    //Functions
+    //=====================
+
+    
+
+
+});
